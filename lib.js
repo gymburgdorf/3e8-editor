c76e8d82161a72716a7011787de90abdec5fe4ff
gymburgdorf <79353439+gymburgdorf@users.noreply.github.com>
2023-03-19 16:52:41 +0000

    Deploying to gh-pages from @ gymburgdorf/monaco-testimport@68ed56bbc75ebbf4fad5968a8dabb68f7f323ea0 🚀

--- lib.js
+++ lib.js
@@ -1,7 +1,7 @@
-function r(n) {
-  return new Promise((t, o) => {
+function c(t) {
+  return new Promise((o, n) => {
     const e = document.createElement("script");
-    e.src = n, e.onload = t, e.onerror = o, document.head.appendChild(e);
+    e.src = t, e.onload = o, e.onerror = n, document.head.appendChild(e);
   });
 }
 document.head.insertAdjacentHTML("beforeend", `
@@ -9,22 +9,24 @@ document.head.insertAdjacentHTML("beforeend", `
     data-name="vs/editor/editor.main"
     href="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.1/min/vs/editor/editor.main.min.css">
 `);
-async function c(n) {
-  await r("https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.1/min/vs/loader.min.js");
-  const t = window.require;
-  t.config({ paths: { vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.1/min/vs" } }), t(["vs/editor/editor.main"], () => {
-    const o = monaco.editor.create(n, {
-      value: `function x() {
-          console.log("Hello world!!!");
-      }`,
-      language: "javascript",
-      theme: "vs-dark",
-      automaticLayout: !0
+async function d(t) {
+  await c("https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.1/min/vs/loader.min.js");
+  const o = window.require;
+  return o.config({ paths: { vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.1/min/vs" } }), new Promise((n) => {
+    o(["vs/editor/editor.main"], () => {
+      const e = monaco.editor.create(t, {
+        value: `function x() {
+                console.log("Hello world!!!");
+            }`,
+        language: "javascript",
+        theme: "vs-dark",
+        automaticLayout: !0
+      });
+      var i = e.getOption(monaco.editor.EditorOption.lineHeight), r = e.getModel().getLineCount(), a = i * r;
+      e.getDomNode().style.height = a + 10 + "px", n(e);
     });
-    var e = o.getOption(monaco.editor.EditorOption.lineHeight), i = o.getModel().getLineCount(), a = e * i;
-    o.getDomNode().style.height = a + 10 + "px";
   });
 }
 export {
-  c as initMonaco
+  d as initMonaco
 };
