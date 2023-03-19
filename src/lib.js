68ed56bbc75ebbf4fad5968a8dabb68f7f323ea0
gymburgdorf <adrian.luethi@gymburgdorf.ch>
2023-03-19 17:52:13 +0100

    return editor

--- src/lib.js
+++ src/lib.js
@@ -20,19 +20,23 @@ export async function initMonaco(element) {
     const require = window.require
     // require is provided by loader.min.js.
     require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.1/min/vs' } });
-    require(["vs/editor/editor.main"], () => {
-        const editor = monaco.editor.create(element, {
-            value: `function x() {
-          console.log("Hello world!!!");
-      }`,
-            language: 'javascript',
-            theme: 'vs-dark',
-            automaticLayout: true
+    return new Promise(resolve=>{
+        require(["vs/editor/editor.main"], () => {
+            const editor = monaco.editor.create(element, {
+                value: `function x() {
+                console.log("Hello world!!!");
+            }`,
+                language: 'javascript',
+                theme: 'vs-dark',
+                automaticLayout: true
+            });
+            // set the editor height based on the number of lines
+            var lineHeight = editor.getOption(monaco.editor.EditorOption.lineHeight);
+            var lineCount = editor.getModel().getLineCount();
+            var height = lineHeight * lineCount;
+            editor.getDomNode().style.height = height + 10 + 'px';
+            resolve(editor)
         });
-        // set the editor height based on the number of lines
-        var lineHeight = editor.getOption(monaco.editor.EditorOption.lineHeight);
-        var lineCount = editor.getModel().getLineCount();
-        var height = lineHeight * lineCount;
-        editor.getDomNode().style.height = height + 10 + 'px';
-    });
+    })
+
 }
