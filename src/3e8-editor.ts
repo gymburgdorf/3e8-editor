e54246c6639a2cdb0778c139c88bfa7f53ea2fbf
gymburgdorf <adrian.luethi@gymburgdorf.ch>
2023-03-24 10:19:17 +0100

    longer template code

--- src/3e8-editor.ts
+++ src/3e8-editor.ts
@@ -2,11 +2,17 @@ import { loadMonaco } from "./loadMonaco";
 
 let monacoEditorPromise = loadMonaco()
 
+const templateCode = `function x() {
+    let a = 1
+    let b = 2
+    return a + b
+}`
+
 export class Editor {
     static async create(element: HTMLElement) {
         let monacoEditor = await monacoEditorPromise
         const editor = monacoEditor.create(element, {
-            value: `function x() {return 2}`,
+            value: templateCode,
             language: 'javascript',
             theme: 'vs-dark',
             automaticLayout: true
