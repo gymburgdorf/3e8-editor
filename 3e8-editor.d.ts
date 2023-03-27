6f124650e7cd1687bd4ba54b1ed432d854becbd2
gymburgdorf <79353439+gymburgdorf@users.noreply.github.com>
2023-03-27 22:07:06 +0000

    Deploying to gh-pages from @ gymburgdorf/monaco-testimport@640d9b2c08881a4121143eb109c9816fd9d40630 🚀

--- 3e8-editor.d.ts
+++ 3e8-editor.d.ts
@@ -1,3 +1,42 @@
-export declare class Editor {
-    static create(element: HTMLElement): Promise<monaco.editor.IStandaloneCodeEditor>;
+export type TMode = "javascript" | "python" | "html";
+export interface IEditorState {
+    element: HTMLElement;
+    mode: TMode;
+    theme: "light" | "dark" | string;
+    fontSize: number;
+    code: string;
+    readOnly: boolean;
+    disableSelect: boolean;
+    showLineNumbers: boolean;
+    minLines: number;
+    maxLines: number;
+    showGutter: boolean;
+    showInvisibles: boolean;
 }
+export interface IEditor {
+    readonly editorState: IEditorState;
+}
+export declare class Editor implements IEditor {
+    readonly editorState: IEditorState;
+    private pythonCodeCheckWorker?;
+    private pythonCodeCheckWorkerBusy?;
+    private parserTimeout?;
+    monacoEditor: monaco.editor.IStandaloneCodeEditor;
+    constructor(config: Partial<IEditorState>, monacoBinding: typeof monaco.editor);
+    static create(config: Partial<IEditorState>): Promise<Editor>;
+}
+/***
+this._beautify =  ace.require("ace/ext/beautify");
+// @check: window.beautifyOptions = this._beautify.options;
+
+
+beautify() {
+  this._beautify.beautify(this.editor.session);
+}
+
+ const langTools = ace.require("ace/ext/language_tools");
+console.log(langTools);
+       enableBasicAutocompletion: true,
+      enableLiveAutocompletion: true,
+      enableSnippets: false
+**/ 
--- 3e8-editor.js
+++ 3e8-editor.js
@@ -1,42 +1,81 @@
-function d(t) {
-  return new Promise((o, i) => {
-    const e = document.createElement("script");
-    e.src = t, e.onload = o, e.onerror = i, document.head.appendChild(e);
+var u = Object.defineProperty;
+var p = (o, e, t) => e in o ? u(o, e, { enumerable: !0, configurable: !0, writable: !0, value: t }) : o[e] = t;
+var i = (o, e, t) => (p(o, typeof e != "symbol" ? e + "" : e, t), t);
+function g(o) {
+  return new Promise((e, t) => {
+    const n = document.createElement("script");
+    n.src = o, n.onload = e, n.onerror = t, document.head.appendChild(n);
   });
 }
-const n = "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.1/min/vs";
+const r = "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.36.1/min/vs";
 document.head.insertAdjacentHTML("beforeend", `
     <link rel="stylesheet"
     data-name="vs/editor/editor.main"
-    href="${n}/editor/editor.main.min.css">
+    href="${r}/editor/editor.main.min.css">
 `);
-async function s() {
-  await d(`${n}/loader.min.js`);
-  const t = window.require;
-  return t.config({ paths: { vs: n } }), new Promise((o) => {
-    t(["vs/editor/editor.main"], () => {
-      o(monaco.editor);
+async function y() {
+  await g(`${r}/loader.min.js`);
+  const o = window.require;
+  return o.config({ paths: { vs: r } }), new Promise((e) => {
+    o(["vs/editor/editor.main"], () => {
+      e(monaco.editor);
     });
   });
 }
-let l = s();
-const m = `function x() {
+let f = y();
+const v = `function x() {
     let a = 1
     let b = 2
     return a + b
 }`;
-class u {
-  static async create(o) {
-    const e = (await l).create(o, {
-      value: m,
+class a {
+  constructor(e, t) {
+    i(this, "editorState");
+    // private readonly mode: "python" | "javascript" | undefined;
+    // private _maxLines: number | undefined;
+    // private readonly editor: AceAjax.Editor;
+    // private _beautify: any;
+    i(this, "pythonCodeCheckWorker");
+    i(this, "pythonCodeCheckWorkerBusy");
+    i(this, "parserTimeout");
+    i(this, "monacoEditor");
+    this.editorState = Object.assign({
+      element: document.getElementById("editor") || document.createElement("div"),
+      mode: "python",
+      theme: "dark",
+      fontSize: 18,
+      code: "",
+      readOnly: !1,
+      disableSelect: !1,
+      showLineNumbers: !["html", "css", "svg"].includes(e.mode || ""),
+      minLines: 4,
+      showInvisibles: e.mode === "python",
+      maxLines: 20,
+      showGutter: !0
+    }, e);
+    const { code: n, element: s, minLines: w, maxLines: b, theme: E, mode: C, showGutter: d, showLineNumbers: c, readOnly: L, fontSize: S, showInvisibles: k } = this.editorState;
+    this.monacoEditor = t.create(s, {
+      value: v,
       language: "javascript",
       theme: "vs-dark",
-      automaticLayout: !0
+      automaticLayout: !0,
+      ...(!d || !c) && {
+        lineNumbers: "off",
+        glyphMargin: !1,
+        folding: !1,
+        lineDecorationsWidth: 0,
+        lineNumbersMinChars: 0
+      },
+      renderLineHighlight: "none"
     });
-    var r = e.getOption(monaco.editor.EditorOption.lineHeight), a = e.getModel().getLineCount(), c = r * a;
-    return e.getDomNode().style.height = c + 10 + "px", e;
+    var m = this.monacoEditor.getOption(monaco.editor.EditorOption.lineHeight), l = this.monacoEditor.getModel().getLineCount(), h = m * l;
+    this.monacoEditor.getDomNode().style.height = h + 10 + "px";
+  }
+  static async create(e) {
+    let t = await f;
+    return new a(e, t);
   }
 }
 export {
-  u as Editor
+  a as Editor
 };
